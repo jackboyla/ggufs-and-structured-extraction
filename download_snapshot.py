803d35489af766010b2de2654706708549717c9c
Jack Boylan <boylan.jack@gmail.com>
2024-12-07 15:03:03 +0000

    Updates download snapshot script

--- download_snapshot.py
+++ download_snapshot.py
@@ -1,6 +1,6 @@
 from huggingface_hub import snapshot_download
 import os
-os.environ['HF_TOKEN'] = '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'
+os.environ['HF_TOKEN'] = '<<YOUR-TOKEN>>'
 model_id="EmergentMethods/Phi-3-mini-4k-instruct-graph"
 snapshot_download(repo_id=model_id, local_dir="Phi-3-mini-4k-instruct-graph",
                   local_dir_use_symlinks=False, revision="main")
